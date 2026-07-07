f80f1da143934196c323f9f11bcf079a8f82570f
Kozinak <yroslavbot1@gmail.com>
2026-07-07 22:19:02 +0300

    Function for reqest model list

--- AiMafia/Views/MainWindow.axaml.cs
+++ AiMafia/Views/MainWindow.axaml.cs
@@ -1,16 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 using AiMafia.Models;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
 namespace AiMafia.Views;
 
+
 public partial class MainWindow : Window
 {
     public string key = "";
+    
+    private class resp
+    {public List<OpenRouterModel> data; }
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public async Task<string> getModelList()
+    {
+        using HttpClient client = new HttpClient();
+        try
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://openrouter.ai/api/v1/models?category=roleplay");
+            request.Headers.Add("Authorization", $"Bearer {key}");
+            var answ = await client.SendAsync(request);
+            var responseBody = await answ.Content.ReadAsStringAsync();
+            var rootNode = JsonNode.Parse(responseBody);
+            List<OpenRouterModel> data = rootNode["data"].Deserialize<List<OpenRouterModel>>();
+            foreach(var item in data) Console.WriteLine(item.url);
+            return responseBody;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return string.Empty;
+        }
     }
 
     //Test function
