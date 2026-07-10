ac62469b69e5112ffdaf50a0cc131f30d51ac52f
Kozinak <yroslavbot1@gmail.com>
2026-07-10 10:26:44 +0300

    Fix viewmodel error

--- AiMafia/ViewModels/MainWindowViewModel.cs
+++ AiMafia/ViewModels/MainWindowViewModel.cs
@@ -7,17 +7,5 @@ namespace AiMafia.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public partial class PlayerRow : ObservableObject
-    {
-        [ObservableProperty]
-        private int _playerNumber;
-
-        [ObservableProperty]
-        private OpenRouterModel _selectedModel;
-
-        public PlayerRow(int number)
-        {
-            PlayerNumber = number;
-        }
-    }
+    ObservableCollection<OpenRouterModel> ViewAiModels { get; } = new ObservableCollection<OpenRouterModel>() ;
 }
